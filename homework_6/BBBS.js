a25bfea190942b2ed816de7dbc03ea3509b0d5a3
Yihong <isopreneshi@gmail.com>
2018-10-26 03:49:19 -0400

    Tried local storage

--- homework_6/BBBS.js
+++ homework_6/BBBS.js
@@ -1,12 +1,30 @@
 $(function(){
- 
-   // 开始写 jQuery 代码...
 
    $(".Qty-options").click(function(){
    		
-   		$(this).css("border","3px solid #2C2726");
+   		//$(this).css("border","3px solid #2C2726");
+   		$(this).toggleClass("Border");
    		
    	});
+
+   
+	
+
+ 	 // get the savedAnimal in local storage if one exists
+  	/**var Qty = JSON.parse(localStorage.getItem("Qty"));**/
+
+  	$("#Qty-1").click(function(){
+  		localStorage.setItem("Qty", JSON.stringify("1"));
+  		$(".item-picture-detail").attr("src", "i/01-Original.jpg");
+  		$(".item-price").text("$2.99");
+  	});
+
+  	$("#Qty-3").click(function(){
+  		localStorage.setItem("Qty", JSON.stringify("3"));
+  		$(".item-picture-detail").attr("src", "i/Background-image.jpg");
+  		$(".item-price").text("$8.97");
+  	});
+	
    
  
 });
